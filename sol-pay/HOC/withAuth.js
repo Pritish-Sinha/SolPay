4e49b688f96810f41b7e4f8db21b082bc8d94971
Pritish Sinha <bpb1789@gmail.com>
2022-09-10 07:03:58 +0000

    bug fix

--- sol-pay/HOC/withAuth.js
+++ sol-pay/HOC/withAuth.js
@@ -10,7 +10,7 @@ const withAuth = (WrappedComponent) => {
       // If there is no access token we redirect to "/" page.
       if (!pKey) {
         Router.replace("/");
-        return nulgit l;
+        return nulgit;
       }
 
       // If this is an accessToken we just render the component that was passed with all its props
