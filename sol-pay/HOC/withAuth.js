a0826d0cd04bfeed55cdc2e4b8ccc6b88681cd11
Pritish Sinha <bpb1789@gmail.com>
2022-09-10 07:02:19 +0000

    wallet id save

---
+++ sol-pay/HOC/withAuth.js
@@ -0,0 +1,26 @@
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+
+const withAuth = (WrappedComponent) => {
+  return (props) => {
+    if (typeof window !== "undefined") {
+      const Router = useRouter();
+      const pKey = Cookies.get("publicKey");
+
+      // If there is no access token we redirect to "/" page.
+      if (!pKey) {
+        Router.replace("/");
+        return nulgit l;
+      }
+
+      // If this is an accessToken we just render the component that was passed with all its props
+
+      return <WrappedComponent {...props} />;
+    }
+
+    // If we are on server, return null
+    return null;
+  };
+};
+
+export default withAuth;
